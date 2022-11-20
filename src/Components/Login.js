37c649f9473a916bb586b7a8e90b925bce939f2d
shivani_simpleDev <shivi.solanki29@gmail.com>
2022-11-20 07:28:00 +0100

    add css

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,23 +3,23 @@ import React from "react";
 const Login = () => {
   return (
     <>
-    <div class="page-login">
-
-      <form class="ui_form" method="POST">
-        <div class="field">
-          <label>Username </label>
-          <input type="text" name="username" placeholder="Username"  />
-        </div>
-        <div class="field">
-          <label>Password </label>
-          <input type="password" name="pass" placeholder="Password" />
-        </div>
-        <button class="ui_primary_labeled_icon_button" type="submit">
-          <i class="unlock alternate icon"></i>
-          Login
-        </button>
-      </form>
-    </div>
+      <div class="page-login">
+        <form class="ui_form" method="POST">
+          <header>
+            <h3>Quiz App </h3>
+          </header>
+          <div class="field">
+            <input type="email" name="email" placeholder="Email" />
+          </div>
+          <div class="field">
+            <input type="text" name="name" placeholder="Name" />
+          </div>
+          <button class="ui_primary_labeled_icon_button" type="submit">
+            <i class="unlock alternate icon"></i>
+            Start
+          </button>
+        </form>
+      </div>
     </>
   );
 };
