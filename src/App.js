3c74a253364097be86f4ca4d92483b04208db7e4
shivani_simpleDev <shivi.solanki29@gmail.com>
2022-11-21 12:09:22 +0100

    add lgoin field

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Login from "./Components/Login";
 
+
 function App() {
+
   return (
     <>
-      <hearder>Hello Nishka</hearder>
       <Login />
     </>
   );
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,21 +1,60 @@
 import React from "react";
+import { useState } from "react";
 
 const Login = () => {
+
+const [values, setValues] = useState({name:'', email:''})
+// const [errors, setErrors] =useState({})
+
+const handleChange =(e) =>{
+    const {name, value} = e.target
+    setValues({...values,
+        [name]:value
+    })
+}
+const handleSubmit =(e) =>{
+    e.preventDefault();
+    // if(validate())
+    console.log(values)
+}
+
+// const validate =() =>{
+//     let temp ={}
+//     temp.email = (/\S+@\S+\.\S+/).test(values.email) ? "" : "Email is not valid."
+//     temp.name = (values.name !=="" )? "" : "This field is required."
+//     setErrors(temp)
+//     console.log(temp)
+//     return Object.values(temp).every(x => x === "")
+// }
   return (
-    <>
-      <div class="page-login">
-        <form class="ui_form" method="POST">
-          <header>
+    <> 
+      <div className="page-login">
+        <form className="ui_form" method="post" onSubmit={handleSubmit}>
+          <header> 
             <h3>Quiz App </h3>
           </header>
-          <div class="field">
-            <input type="email" name="email" placeholder="Email" />
+          <div className="field">
+            <input 
+            type="email" 
+            name="email" 
+            placeholder="Email"
+            value={values.email}
+            onChange= {handleChange}
+            // {...(errors.email && {error:true, helpertext:errors.email})}
+            />
           </div>
-          <div class="field">
-            <input type="text" name="name" placeholder="Name" />
+          <div className="field">
+            <input 
+            type="text" 
+            name="name" 
+            placeholder="Name" 
+            value={values.name}
+            onChange={handleChange}
+            // {...(errors.name && {error:true, helpertext:errors.name})}
+            />
           </div>
-          <button class="ui_primary_labeled_icon_button" type="submit">
-            <i class="unlock alternate icon"></i>
+          <button className="ui_primary_labeled_icon_button" type="submit">
+            <i className="unlock alternate icon"></i>
             Start
           </button>
         </form>
